01d441ee1935c97fc77d07f3182028bd383a7755
wang1897b7@qq.com <wang760328267>
2016-07-04 22:33:48 +0800

    updata

---
+++ app/src/main/java/com/wsd/text/pict_can/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wsd.text.pict_can;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
