e2b60a6bb5bfbc3150d517bcd333320a60eb29c2
Sun <wang1897b7@qq.com>
2016-07-05 17:31:31 +0800

    new_create_pacakage

---
+++ app/src/main/java/com/wsd/text/pict_can/ui/activity/Register.java
@@ -0,0 +1,15 @@
+package com.wsd.text.pict_can.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wsd.text.pict_can.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/wsd/text/pict_can/ui/text.java
@@ -0,0 +1,7 @@
+package com.wsd.text.pict_can.ui;
+
+/**
+ * Created by Sun on 2016/7/5.
+ */
+public class text {
+}
