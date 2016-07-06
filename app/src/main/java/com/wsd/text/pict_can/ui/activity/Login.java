eabca723f426f4be6ab5b8e4f6e1e94403ecdbfc
wang1897b7@qq.com <wang760328267>
2016-07-06 22:48:40 +0800

    updata

---
+++ app/src/main/java/com/wsd/text/pict_can/ui/activity/Login.java
@@ -0,0 +1,43 @@
+package com.wsd.text.pict_can.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.wsd.text.pict_can.R;
+
+public class Login extends Activity {
+
+    private EditText et_user;
+    private EditText et_pass;
+    private Button btn_sub;
+    private TextView tv_findPass;
+    private TextView tv_register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init(){
+        et_pass= (EditText) findViewById(R.id.login_pass);
+        et_user= (EditText) findViewById(R.id.login_user);
+
+        tv_findPass= (TextView) findViewById(R.id.login_tvFindPass);
+        tv_register= (TextView) findViewById(R.id.login_tvRegister);
+        btn_sub= (Button) findViewById(R.id.login_btn);
+
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
