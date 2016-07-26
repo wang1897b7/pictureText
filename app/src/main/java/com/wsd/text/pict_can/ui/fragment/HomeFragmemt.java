d8c51aa96fee5e1a7ac7a28ad6526cb8eb9ea1eb
Sun <wang1897b7@qq.com>
2016-07-26 19:30:07 +0800

    initBanner_Showfragment_commit_0726

--- app/src/main/java/com/wsd/text/pict_can/ui/fragment/HomeFragmemt.java
+++ app/src/main/java/com/wsd/text/pict_can/ui/fragment/HomeFragmemt.java
@@ -6,13 +6,21 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.wsd.text.pict_can.R;
 import com.wsd.text.pict_can.adapter.ViewPagerAdapter;
 import com.wsd.text.pict_can.view.SlidingTabLayout;
+
+import java.util.List;
+
+import cn.lightsky.infiniteindicator.InfiniteIndicatorLayout;
+import cn.lightsky.infiniteindicator.slideview.BaseSliderView;
+import cn.lightsky.infiniteindicator.slideview.DefaultSliderView;
 
 /**
  * Created by Sun on 2016/7/14.
@@ -23,12 +31,13 @@ public class HomeFragmemt extends BaseFragment{
     private String[] titles = new String[]{"Sample Tab 1", "Sample Tab 2", "Sample Tab 3", "Sample Tab 4"
             , "Sample Tab 5", "Sample Tab 6", "Sample Tab 7", "Sample Tab 8"};
     private Toolbar toolbar;
-
+    View mView;
     SlidingTabLayout slidingTabLayout;
+    private FrameLayout topBanner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View mView = inflater.inflate(R.layout.fragment_home, container, false);
+         mView = inflater.inflate(R.layout.fragment_home, container, false);
         pager = (ViewPager)mView. findViewById(R.id.viewpager);
         slidingTabLayout = (SlidingTabLayout)mView. findViewById(R.id.sliding_tabs);
         init();
@@ -38,7 +47,7 @@ public class HomeFragmemt extends BaseFragment{
     private void init(){
 
         pager.setAdapter(new ViewPagerAdapter(getFragmentManager(), titles));
-
+        topBanner= (FrameLayout)  mView.findViewById(R.id.top_banner);
         slidingTabLayout.setViewPager(pager);
         slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
@@ -46,5 +55,29 @@ public class HomeFragmemt extends BaseFragment{
                 return Color.WHITE;
             }
         });
+    }
+
+    private void initBanner(final List<String> banners, int inteval) {
+        topBanner.removeAllViews();
+        InfiniteIndicatorLayout indicator = new InfiniteIndicatorLayout(getActivity());
+        for (int i = 0, size = banners.size(); i < size; i++) {
+           // final Banner banner = banners.get(i);
+            DefaultSliderView sliderView = new DefaultSliderView(getActivity());
+            DefaultSliderView sliderView = new DefaultSliderView(getActivity());
+            sliderView.image("www.baidu.com")
+                    .setScaleType(BaseSliderView.ScaleType.FitCenter)
+                    .showImageResForEmpty(R.drawable.default_bg)
+                    .showImageResForError(R.drawable.default_bg)
+                    .setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                        @Override
+                        public void onSliderClick(BaseSliderView slider) {
+                          //  AppUtil.jumpToInfo(getActivity(), banner);
+                        }
+                    });
+            indicator.addSlider(sliderView);
+        }
+        indicator.startAutoScroll(inteval * 1000);
+        indicator.setIndicatorPosition(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
+        topBanner.addView(indicator);
     }
 }
